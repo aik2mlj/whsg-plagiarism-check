6a0210a7dff3cd1eb118d1a6360c17700adb2d25
aik2 <aik2mlj@gmail.com>
2023-11-16 02:17:43 +0400

    get algorithm done

--- get_all_mel_latent.py
+++ get_all_mel_latent.py
@@ -1,5 +1,8 @@
 import os
 
+import torch
+import utils
+
 from tqdm import tqdm
 
 from data_utils.dataset import (
@@ -10,6 +13,8 @@ from data_utils.dataset import (
     load_split_file,
     read_data,
 )
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
 
 def get_train_val_mels():
@@ -33,16 +38,108 @@ def get_train_val_mels():
             song_name=folder_name,
             label=label
         )
-        # analysis = song.analyze()
+        # total measure, num beat per measure
+        n_bar, nbpm = song.total_measure, song.num_beat_per_measure
+        if nbpm == 3:
+            continue
 
         if i in train_ids:
-            train_mels.append(song.melody)
+            train_mels.append((song.melody, n_bar))
         else:
-            valid_mels.append(song.melody)
+            valid_mels.append((song.melody, n_bar))
 
-    print(len(train_mels), len(valid_mels))
+    print(f"train set: {len(train_mels)}, val set: {len(valid_mels)}")
     return train_mels, valid_mels
 
 
+def get_melprmat(mels, seg_len=2):
+    prmats = []
+    for mel, num_bar in tqdm(mels, desc="nmat to melprmat"):
+        # get 1-bar melprmat
+        prmat = utils.nmat_to_melprmat(mel, num_bar)
+        # rolling
+        prmat_rolls = [prmat]
+        for i in range(1, seg_len):
+            prmat_rolls.append(torch.roll(prmat, -i, dims=0))
+        # here is the seg with hopsize=1
+        prmat_seg = torch.cat(prmat_rolls, dim=1)[:, :-(seg_len - 1), :]
+
+        prmats.append(prmat_seg)
+    prmats = torch.concat(prmats)
+    print(prmats.shape)
+
+    # clean empty 2bars
+    print("cleaning empty 2-bars...")
+    empty_index = torch.abs(prmats).sum(dim=(1, 2)) > 0
+    prmats = prmats[empty_index]
+    print(prmats.shape)
+
+    return prmats.to(device)
+
+
+def get_melchroma(mels, seg_len=2):
+    prmats = []
+    for mel, num_bar in tqdm(mels, desc="nmat to melchroma"):
+        # get 1-bar melchroma
+        prmat = utils.nmat_to_melchroma(mel, num_bar)
+        # rolling
+        prmat_rolls = [prmat]
+        for i in range(1, seg_len):
+            prmat_rolls.append(torch.roll(prmat, -i, dims=0))
+        # here is the seg with hopsize=1
+        prmat_seg = torch.cat(prmat_rolls, dim=1)[:-(seg_len - 1), :, :]
+
+        prmats.append(prmat_seg)
+    prmats = torch.concat(prmats)
+    print(prmats.shape)
+
+    # clean empty 2bars
+    print("cleaning empty 2-bars...")
+    empty_index = torch.abs(prmats).sum(dim=(1, 2)) > 0
+    prmats = prmats[empty_index]
+    print(prmats.shape)
+
+    return prmats
+
+
+def compute_notewise_copy_ratio(mel, train_mels):
+    """
+    mel, train_mels should be melchroma #[:, 16*seg_len, 12]
+    """
+    # truncate melprmat to only onset & pitch, without rest & sustain
+    train_size = train_mels.shape[0]
+
+    ratios = []
+    for mel_2bar in tqdm(mel):
+        mel_expand = mel_2bar.unsqueeze(0).expand(train_size, -1, -1)
+
+        # notes the given melody contains
+        num_mel_notes = torch.sum(mel_expand == 1, dim=(1, 2))
+        # notes the training set contains
+        num_train_notes = torch.sum(train_mels == 1, dim=(1, 2))
+
+        # transpose to 12 keys
+        ratio = -1.
+        for i in range(12):
+            train_transpose = torch.roll(train_mels, i, dims=2)
+            # notes that the given melody match with the training set (pitch & onset identical)
+            num_copy = torch.sum(
+                (mel_expand == train_transpose) * (mel_expand == 1), dim=(1, 2)
+            )
+            ratio = max(
+                ratio, (num_copy / (num_train_notes + num_mel_notes)).max().item()
+            )
+        ratios.append(ratio)
+    print(ratios)
+    ratio = sum(ratios) / len(ratios)
+    print(ratio)
+    return ratio
+
+
 if __name__ == "__main__":
-    pass
+    train_mels, val_mels = get_train_val_mels()
+    train_melchroma = get_melchroma(train_mels)
+    val_melchroma = get_melchroma([val_mels[0]])
+
+    print(compute_notewise_copy_ratio(val_melchroma, train_melchroma))
+    exit(0)
---
+++ utils.py
@@ -0,0 +1,264 @@
+import muspy
+import pretty_midi as pm
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+import music21
+
+BIN = 4
+
+
+def get_music(midi_path):
+    music = muspy.read_midi(midi_path)
+    music.adjust_resolution(BIN)
+    if len(music.time_signatures) == 0:
+        music.time_signatures.append(muspy.TimeSignature(0, 4, 4))
+    return music
+
+
+def get_note_matrix(music, tracks=[0]):
+    """
+    get note matrix: same format as pop909-4-bin
+        for piano, this function simply extracts notes of given tracks
+    """
+    notes = []
+    for inst_idx in tracks:
+        inst = music.tracks[inst_idx]
+        for note in inst.notes:
+            onset = int(note.time)
+            duration = int(note.duration)
+            if onset >= 0:
+                # this is compulsory because there may be notes
+                # with zero duration after adjusting resolution
+                if duration == 0:
+                    duration = 1
+                notes.append([
+                    onset,
+                    note.pitch,
+                    duration,
+                ])
+    # sort according to (start, duration)
+    # notes.sort(key=lambda x: (x[0] * BIN + x[1], x[2]))
+    notes.sort(key=lambda x: (x[0], x[1], x[2]))
+    return notes
+
+
+def nmat_to_prmat(nmat, num_2bar, n_step=32):
+    prmat = torch.zeros((num_2bar, n_step, 128))
+    for o, p, d in nmat:
+        n_2bar = o // n_step
+        if n_2bar >= num_2bar:
+            break
+        o_2bar = o % n_step
+        prmat[n_2bar, o_2bar, p] = d
+    return prmat
+
+
+def nmat_to_melprmat(nmat, num_bar, n_step=16):  # only 1 bar!
+    prmat_ec2vae = torch.zeros((num_bar * n_step, 130))
+    t = 0
+    for o, p, d in nmat:
+        if o >= num_bar * n_step:
+            break
+        if o > t:
+            for i in range(t + 1, o):
+                prmat_ec2vae[i, 129] = 1  # rest
+        prmat_ec2vae[o, p] = 1
+        for i in range(o + 1, min(o + d, num_bar * n_step)):
+            prmat_ec2vae[i, 128] = 1
+        t = o + d
+    prmat_ec2vae = prmat_ec2vae.reshape((num_bar, n_step, 130))
+    return prmat_ec2vae
+
+
+def nmat_to_melchroma(nmat, num_bar, n_step=16):  # only 1 bar!
+    """pitch % 12, without sustain & rest"""
+    prmat = torch.zeros((num_bar * n_step, 12))
+    for o, p, d in nmat:
+        if o >= num_bar * n_step:
+            break
+        prmat[o, p % 12] = 1
+    prmat = prmat.reshape((num_bar, n_step, 12))
+    return prmat
+
+
+def prmat_to_midi_file(prmat, fpath, labels=None):
+    # prmat: (B, 32, 128)
+    if "Tensor" in str(type(prmat)):
+        prmat = prmat.cpu().detach().numpy()
+    midi = pm.PrettyMIDI()
+    piano_program = pm.instrument_name_to_program("Acoustic Grand Piano")
+    piano = pm.Instrument(program=piano_program)
+    t = 0
+    n_step = prmat.shape[1]
+    t_bar = int(n_step / 8)
+    for bar_ind, bars in enumerate(prmat):
+        for step_ind, step in enumerate(bars):
+            for key, dur in enumerate(step):
+                dur = int(dur)
+                if dur > 0:
+                    note = pm.Note(
+                        velocity=80,
+                        pitch=key,
+                        start=t + step_ind * 1 / 8,
+                        end=min(t + (step_ind + int(dur)) * 1 / 8, t + t_bar),
+                    )
+                    piano.notes.append(note)
+        t += t_bar
+    midi.instruments.append(piano)
+    if labels is not None:
+        midi.lyrics.clear()
+        t = 0
+        for label in labels:
+            midi.lyrics.append(pm.Lyric(label, t))
+            t += t_bar
+    midi.write(fpath)
+
+
+def reduce_chd_quality(chd):  # 12
+    chord = music21.chord.Chord(chd)
+    chd_red = []
+    root = None
+    if chord.root() is not None:
+        root = chord.root().pitchClass
+        chd_red.append(root)
+    if chord.third is not None:
+        third = chord.third.pitchClass
+        chd_red.append(third)
+    if chord.fifth is not None:
+        fifth = chord.fifth.pitchClass
+        chd_red.append(fifth)
+    if chord.semitonesFromChordStep(2) is not None:
+        if chord.third is None:
+            second = (chord.semitonesFromChordStep(2) + root) % 12
+            chd_red.append(second)
+    if chord.semitonesFromChordStep(4) is not None:
+        if chord.third is None:
+            fourth = (chord.semitonesFromChordStep(4) + root) % 12
+            chd_red.append(fourth)
+    return chd_red, root
+
+
+def get_chord(music, num_2bar, tracks=[1]):
+    """
+    track indicates the chord track in the midi file
+    """
+    chd_nmat = get_note_matrix(music, tracks)
+
+    chd = torch.zeros((num_2bar * 8, 36))
+    idx = 0
+    while idx < len(chd_nmat):
+        t = chd_nmat[idx][0]
+        d = chd_nmat[idx][2]
+        ps = []
+        while idx < len(chd_nmat) and chd_nmat[idx][0] == t:
+            ps.append(chd_nmat[idx][1])
+            idx += 1
+        if len(ps) == 1:
+            continue  # only bass changed, ignore
+
+        tmp_chd = [p % 12 for p in ps[1 :]]
+        tmp_chd, root = reduce_chd_quality(tmp_chd)
+
+        if root is None:
+            root = ps[0] % 12
+        bass = root
+
+        bar = t // 4
+        d = min(d // 4, num_2bar * 8 - bar)
+        chd[bar : bar + d, root] = 1
+        chd[bar : bar + d, 24 + bass] = 1
+
+        for p in tmp_chd:
+            chd[bar : bar + d, 12 + p] = 1
+    chd = chd.reshape((num_2bar, 8, 36))
+    return chd
+
+
+def get_chord_ec2vae(music, num_2bar, tracks=[1]):
+    """
+    track indicates the chord track in the midi file
+    """
+    chd_nmat = get_note_matrix(music, tracks)
+
+    chd = torch.zeros((num_2bar * 32, 12))
+    idx = 0
+    while idx < len(chd_nmat):
+        t = chd_nmat[idx][0]
+        d = chd_nmat[idx][2]
+        ps = []
+        while idx < len(chd_nmat) and chd_nmat[idx][0] == t:
+            ps.append(chd_nmat[idx][1])
+            idx += 1
+        if len(ps) == 1:
+            continue  # only bass changed, ignore
+
+        tmp_chd = [p % 12 for p in ps[1 :]]
+        tmp_chd = reduce_chd_quality(tmp_chd)
+
+        for p in tmp_chd:
+            chd[t : min(num_2bar * 32, t + d), p] = 1
+    chd = chd.reshape((num_2bar, 32, 12))
+    return chd
+
+
+def chd_to_midi_file(chords, output_fpath, one_beat=0.5):
+    """
+    retrieve midi from chords
+    """
+    if "Tensor" in str(type(chords)):
+        chords = chords.cpu().detach().numpy()
+    midi = pm.PrettyMIDI()
+    piano_program = pm.instrument_name_to_program("Acoustic Grand Piano")
+    piano = pm.Instrument(program=piano_program)
+    t = 0.0
+    for seg in chords:
+        for beat, chord in enumerate(seg):
+            if chord.shape[0] == 14:
+                root = int(chord[0])
+                chroma = chord[1 : 13].astype(int)
+                bass = int(chord[13])
+            elif chord.shape[0] == 36:
+                root = int(chord[0 : 12].argmax())
+                chroma = chord[12 : 24].astype(int)
+                bass = int(chord[24 :].argmax())
+
+            chroma = np.roll(chroma, -bass)
+            c3 = 48
+            for i, n in enumerate(chroma):
+                if n == 1:
+                    note = pm.Note(
+                        velocity=80,
+                        pitch=c3 + i + bass,
+                        start=t * one_beat,
+                        end=(t + 1) * one_beat,
+                    )
+                    piano.notes.append(note)
+            t += 1
+
+    midi.instruments.append(piano)
+    midi.write(output_fpath)
+
+
+def phrase_config_from_string(phrase_annotation):
+    index = 0
+    phrase_configuration = []
+    while index < len(phrase_annotation):
+        label = phrase_annotation[index]
+        index += 1
+        n_bars = ""
+        while index < len(phrase_annotation) and phrase_annotation[index].isdigit():
+            n_bars += phrase_annotation[index]
+            index += 1
+        phrase_configuration.append((label, int(n_bars)))
+    return phrase_configuration
+
+
+def show_matrix(mat):
+    matrix_np = mat.detach().numpy()
+
+    # Plot the matrix
+    plt.imshow(matrix_np, cmap="viridis", interpolation="nearest")
+    plt.colorbar()  # Add a color bar for reference
+    plt.title("Matrix Visualization")
+    plt.show()
